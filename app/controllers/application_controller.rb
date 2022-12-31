86dc9dfc16e6e54ea4f871dcf9481c5be342c4eb
Alexis Zaffalon <67122137+AZaffalon@users.noreply.github.com>
2022-12-31 14:36:47 +0100

    Add User and Company models + Handle turbo stream security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, unless: :devise_controller?
+
+  private
+
+  def current_company
+    @current_company ||= current_user.company if user_signed_in?
+  end
+  # Declare controller method as helper to be used in all views
+  helper_method :current_company
 end
